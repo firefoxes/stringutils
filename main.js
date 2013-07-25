b64a9f04b10eef5451b5e8c03f2768b4b1427b4c
Myles Byrne <myles@myles.id.au>
2013-07-25 12:15:26 -0500

    stringutils as a packaged module

---
+++ main.js
@@ -0,0 +1,7 @@
+var stringUtils = require('./stringutils')
+
+console.log(stringUtils.upperCase("hello"))
+console.log(stringUtils.lowerCase("BYe"))
+
+// exports can only return objects
+// module.exports can return anything (function, string, etc.)
